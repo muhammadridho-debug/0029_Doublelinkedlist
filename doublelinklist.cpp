2b0f14f205ab3bc138bacfb6470691fc5ecd4b66
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-30 20:35:50 +0700

    Membuat fungsi traverse (Ascending)

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -80,4 +80,23 @@ public:
 
         current->next = newNode;   // Step 9d current.next = newnode
     }
+    
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        Node *currentNode = START;
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+            currentNode = currentNode->next;
+            i++;
+        }
+    }
 };
