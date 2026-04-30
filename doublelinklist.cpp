2b8bf5c121252fcc8bee241828252df96d032c90
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-30 20:55:12 +0700

    Menambahkan jeda (pause) dan clear screen

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -240,6 +240,10 @@ int main()
         default:
             cout << "Invalid option\n";
         }
-
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
     } while (choice != '6');
 }
